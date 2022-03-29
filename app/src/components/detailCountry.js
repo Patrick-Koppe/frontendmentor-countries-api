589388f3691a8e3aebea23a83088837067fb3d9f
patrick-koppe <pkoppe@web.de>
2022-03-29 16:41:27 +0200

    Added fontawesome, styling lightbox

--- app/src/components/detailCountry.js
+++ app/src/components/detailCountry.js
@@ -1,6 +1,8 @@
 import './_detailCountry.scss';
 
 import history from '../history';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
 
 
 function DetailCountry(props) {
@@ -36,54 +38,83 @@ function DetailCountry(props) {
             <div className="lightbox fixed">
                 <div className="container">
                     <div className="row">
-                        <div className="col-12">
-                            <button onClick={() => history.push('/')}>Click button back</button>
+                        <div className="col-12 my-5 py-2">
+                            <button className="btn btn-secondary icon left" onClick={() => history.push('/')}>
+                                <FontAwesomeIcon icon={faArrowLeft} />Back</button>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-12 col-md-6">
-                            <img className="card-img-top" src={props.detailCountry.detailCountry[0].flags.svg} alt={`flag of ${props.detailCountry.detailCountry[0].name.common}`}/>
+                    <div className="row justify-content-between">
+                        <div className="col-12 col-md-5">
+                            <img className="card-img-top mb-5 mb-lg-2" src={props.detailCountry.detailCountry[0].flags.svg} alt={`flag of ${props.detailCountry.detailCountry[0].name.common}`}/>
                         </div>
                         <div className="col-12 col-md-6">
-                            <dl>
-                                <div>
-                                    <dt>Native Name:</dt>
-                                    <dd>{props.detailCountry.detailCountry[0].name.official}</dd>
-                                </div>
-                                <div>
-                                    <dt>Population</dt>
-                                    <dd>{numberWithCommas(props.detailCountry.detailCountry[0].population)}</dd>
-                                </div>
-                                <div>
-                                    <dt>Region</dt>
-                                    <dd>{props.detailCountry.detailCountry[0].region}</dd>
-                                </div>
-                                <div>
-                                    <dt>Sub Region</dt>
-                                    <dd>{props.detailCountry.detailCountry[0].subregion}</dd>
-                                </div>
-                                <div>
-                                    <dt>Capital</dt>
-                                    <dd>{props.detailCountry.detailCountry[0].capital}</dd>
-                                </div>
-                                <div>
-                                    <dt>Top Level Domain</dt>
-                                    <dd>{props.detailCountry.detailCountry[0].tld}</dd>
+                            <div className="row">
+
+                                <div className="col-12">
+                                    <dl>
+                                        <dd className="m-0"><h1>{props.detailCountry.detailCountry[0].name.common}</h1></dd>
+                                    </dl>
                                 </div>
 
-                                 <div>
-                                 <dt>Currencies</dt>
-                                 <dd>{currencies(props.detailCountry.detailCountry[0].currencies)}</dd>
-                                 </div>
-                                <div>
-                                    <dt>Languages</dt>
-                                    <dd>{languages(props.detailCountry.detailCountry[0].languages)}</dd>
+                                <div className="col-12 col-md-6">
+                                    <dl>
+                                        <dt>Native Name</dt>
+                                        <dd>{props.detailCountry.detailCountry[0].name.official}</dd>
+                                    </dl>
                                 </div>
-                            </dl>
+                                <div className="col-12 col-md-6">
+                                    <dl>
+                                        <dt>Top Level Domain</dt>
+                                        <dd>{props.detailCountry.detailCountry[0].tld}</dd>
+                                    </dl>
+                                </div>
+
+                                <div className="col-12 col-md-6">
+                                    <dl>
+                                        <dt>Population</dt>
+                                        <dd>{numberWithCommas(props.detailCountry.detailCountry[0].population)}</dd>
+                                    </dl>
+                                </div>
+
+                                <div className="col-12 col-md-6">
+                                    <dl>
+                                        <dt>Currencies</dt>
+                                        <dd>{currencies(props.detailCountry.detailCountry[0].currencies)}</dd>
+                                    </dl>
+                                </div>
+
+                                <div className="col-12 col-md-6">
+                                    <dl>
+                                        <dt>Region</dt>
+                                        <dd>{props.detailCountry.detailCountry[0].region}</dd>
+                                    </dl>
+                                </div>
+
+                                <div className="col-12 col-md-6">
+                                    <dl>
+                                        <dt>Languages</dt>
+                                        <dd>{languages(props.detailCountry.detailCountry[0].languages)}</dd>
+                                    </dl>
+                                </div>
+
+                                <div className="col-12">
+                                    <dl>
+                                        <dt>Sub Region</dt>
+                                        <dd>{props.detailCountry.detailCountry[0].subregion}</dd>
+                                    </dl>
+                                </div>
+
+                                <div className="col-12">
+                                    <dl>
+                                        <dt>Capital</dt>
+                                        <dd>{props.detailCountry.detailCountry[0].capital}</dd>
+                                    </dl>
+                                </div>
+
+                            </div>
                         </div>
                     </div>
                 </div>
-                }
             </div>
         );
     } else {
--- app/src/components/header/header.js
+++ app/src/components/header/header.js
@@ -4,9 +4,15 @@ function Header (props) {
 
     return (
         <header>
-            <div className="container">
-                <h1>Where in the world ?</h1>
-                <div className="mode-switcher">DarkMode</div>
+            <div className="container py-4">
+                <div className="row">
+                    <div className="col-6">
+                        <h3>Where in the world ?</h3>
+                    </div>
+                    <div className="col-6">
+                        <div className="mode-switcher">DarkMode</div>
+                    </div>
+                </div>
             </div>
 
         </header>
